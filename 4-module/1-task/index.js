945452e25bb3e4b0396b95df711dfbf7b351be50
boriskulakov <chfibo11235@gmail.com>
2021-09-08 14:04:00 +0500

    Решил задачу преобразования списка друзей

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,9 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  let friendsList = friends.map(item => `<li>${item.firstName} ${item.lastName}</li>`);
+
+  let ul = document.createElement('ul');
+
+  ul.innerHTML = friendsList.join('');
+
+  return ul;
 }
